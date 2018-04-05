5aaed08049cc4b91c2aff60759552cfd72749cd0
GuMiner <gus.gran@gmail.com>
2018-04-04 20:40:21 -0700

    Starting on curved roads and determining a new approach is necessary.

--- voxelli/roadTypes.go
+++ voxelli/roadTypes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"go-experiments/voxelli/geometry"
 	"go-experiments/voxelli/utils"
+	"math"
 
 	"github.com/go-gl/mathgl/mgl32"
 )
@@ -41,6 +42,7 @@ func (straightRoad StraightRoad) FindBoundary(position mgl32.Vec2, direction mgl
 	currentVector := geometry.NewVector(position, direction)
 
 	// Determine which of the four walls of the grid this vector intersects.
+	// We add in the direction (which is normalized) when we move into new pieces to prevent hitting on the wall we transferred through.
 	left := geometry.NewLineSegment(mgl32.Vec2{0, 0}, mgl32.Vec2{0, gridSize})
 	if doesIntersect, intersectPos := left.Intersects(currentVector); doesIntersect {
 		if straightRoad.rotated {
@@ -96,12 +98,16 @@ func (curvedRoad CurvedRoad) InBounds(position mgl32.Vec2) bool {
 	var length float32
 	switch curvedRoad.rotation % 4 {
 	case 0:
+		// Center == (gridExtent, 0) (down-to-right)
 		length = position.Sub(mgl32.Vec2{gridExtent, 0}).Len()
 	case 1:
+		// Center == (gridExtent, gridExtent) (up-to-right)
 		length = position.Sub(mgl32.Vec2{gridExtent, gridExtent}).Len()
 	case 2:
+		// Center == (0, gridExtent) (left-to-up)
 		length = position.Sub(mgl32.Vec2{0, gridExtent}).Len()
 	case 3:
+		// Center == (0, 0) (left-to-down)
 		length = position.Len()
 	}
 
@@ -112,5 +118,25 @@ func (curvedRoad CurvedRoad) InBounds(position mgl32.Vec2) bool {
 // If the boundary is out-of-bounds, returns (true, {0, 0}, and the relative position of the intersection)
 // If the boundary leads to another piece, returns (false, the offset to the next grid pos, and the relative position of the intersection on that next piece)
 func (curvedRoad CurvedRoad) FindBoundary(position mgl32.Vec2, direction mgl32.Vec2) (inBounds bool, gridId utils.IntVec2, gridPos mgl32.Vec2) {
+	gridSize := float32(GetGridSize()) - 0.5
+	currentVector := geometry.NewVector(position, direction)
+
+	var arc geometry.ArcSegment
+	switch curvedRoad.rotation % 4 {
+	case 0:
+		arc = geometry.NewArcSegment(mgl32.Vec2{gridSize, 0}, gridSize, math.Pi/2, math.Pi)
+	case 1:
+		arc = geometry.NewArcSegment(mgl32.Vec2{gridSize, gridSize}, gridSize, math.Pi, 3*math.Pi/2)
+	case 2:
+		arc = geometry.NewArcSegment(mgl32.Vec2{0, gridSize}, gridSize, 3*math.Pi/2, 2*math.Pi)
+	case 3:
+		arc = geometry.NewArcSegment(mgl32.Vec2{0, 0}, gridSize, 0, math.Pi/2)
+	}
+
+	if intersects, intersectionPoint := arc.Intersects(currentVector); intersects {
+		// For each segment, we need to check two sides to see if the vector is heading to a new piece.
+		return true, utils.IntVec2{0, 0}, intersectionPoint
+	}
+
 	return true, utils.IntVec2{0, 0}, position
 }
