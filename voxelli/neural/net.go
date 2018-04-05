262fe09173a44fd9bc7b7f19794ecaf14e7c695e
GuMiner <gus.gran@gmail.com>
2018-04-04 22:07:42 -0700

    Making the basis of the neural net. Next step -- genetic evaluation and improvements.

---
+++ voxelli/neural/net.go
@@ -0,0 +1,97 @@
+package neural
+
+// See https://github.com/ArztSamuel/Applying_EANNs for the inspiration for this.
+import (
+	"math"
+	"math/rand"
+)
+
+// Defines a deeply-connected neuron with weights to the neurons on the next layer
+type Neuron struct {
+	weights []float32
+}
+
+type NeuralLayer struct {
+	nextLayerSize int
+	neurons       []Neuron
+	bias          float32
+}
+
+type NeuralNet struct {
+	layers []*NeuralLayer
+}
+
+func (n *NeuralNet) Evaluate(inputs []float32) []float32 {
+	for _, layer := range n.layers {
+		inputs = layer.Evaluate(inputs)
+	}
+
+	return inputs
+}
+
+func NewNeuralNet(layerSizes []int, outputCount int) *NeuralNet {
+	net := NeuralNet{layers: []*NeuralLayer{}}
+	for i, layerSize := range layerSizes {
+		nextLayerSize := outputCount
+		if i != len(layerSizes)-1 {
+			nextLayerSize = layerSizes[i+1]
+		}
+
+		net.layers = append(net.layers, newNeuralLayer(layerSize, nextLayerSize))
+	}
+
+	return &net
+}
+
+// Apply the sigmoid function as an activation function
+func activationFunction(value float32) float32 {
+	if value > 10 {
+		return 1.0
+	} else if value < -10 {
+		return 0.0
+	}
+
+	return 1.0 / (1.0 + float32(math.Exp(-float64(value))))
+}
+
+// Activates using inputs for these neurons and returns the output for the next layer
+// len(inputs) == len(neurons). len(output) == nextLayerSize
+func (n *NeuralLayer) Evaluate(inputs []float32) []float32 {
+	outputs := make([]float32, n.nextLayerSize)
+	for i := 0; i < n.nextLayerSize; i++ {
+		outputs[i] = n.bias
+	}
+
+	for i, input := range inputs {
+		for j, weight := range n.neurons[i].weights {
+			outputs[j] += weight * input
+		}
+	}
+
+	for i := 0; i < n.nextLayerSize; i++ {
+		outputs[i] = activationFunction(outputs[i])
+	}
+
+	return outputs
+}
+
+func newNeuralLayer(layerSize, nextLayerSize int) *NeuralLayer {
+	neurons := make([]Neuron, layerSize)
+	for i, _ := range neurons {
+		neurons[i].weights = make([]float32, nextLayerSize)
+	}
+
+	layer := NeuralLayer{nextLayerSize: nextLayerSize, neurons: neurons, bias: 1.0}
+	layer.Randomize()
+
+	return &layer
+}
+
+// Randomizes all weights, excluding the bias.
+func (n *NeuralLayer) Randomize() {
+	for i, _ := range n.neurons {
+		for j, _ := range n.neurons[i].weights {
+			n.neurons[i].weights[j] = rand.Float32()*2.0 - 1.0
+		}
+	}
+}
